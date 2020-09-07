ffae3772923b0567e4271614c3e8c0207f9684fb
Trex_Codes <30562611+juan1305@users.noreply.github.com>
2020-09-06 20:57:43 -0500

    1.0 ADD files(Proyect complete) this need to fix some bugs

---
+++ Script1.js
@@ -0,0 +1,8 @@
+// cuadroColor
+
+function ColorCuadro() {
+	const callColorSun = document.getElementById('cuadroColor');
+	callColorSun.style.display = 'inherit';
+	callColorSun.style.visibility = 'visible';
+
+}
---
+++ Script3.js
@@ -0,0 +1,81 @@
+function S2Call() {
+	var CallcanvasSun = document.getElementById('cuadroSun3');
+		CallcanvasSun.style.visibility = 'visible';
+		CallcanvasSun.style.display = 'block';
+}
+
+// call colors and and funcions
+//son2
+
+document.getElementById('s2color1').onclick = function() {
+	var calls2color1_1 = document.getElementById('fa1_sub1');
+	var calls2color1_2 = document.getElementById('fa1_Sub2');
+	var calls2color1_3 = document.getElementById('fa2_sub1');
+	var calls2color1_4 = document.getElementById('fa2_Sub2');
+	calls2color1_1.style.backgroundColor = 'red';
+	calls2color1_2.style.backgroundColor = 'red';
+	calls2color1_3.style.backgroundColor = 'red';
+	calls2color1_4.style.backgroundColor = 'red';
+}
+
+
+document.getElementById('s2color2').onclick = function() {
+	var calls2color2_1 = document.getElementById('fa1_sub1');
+	var calls2color2_2 = document.getElementById('fa1_Sub2');
+	var calls2color2_3 = document.getElementById('fa2_sub1');
+	var calls2color2_4 = document.getElementById('fa2_Sub2');
+	calls2color2_1.style.backgroundColor = 'blue';
+	calls2color2_2.style.backgroundColor = 'blue';
+	calls2color2_3.style.backgroundColor = 'blue';
+	calls2color2_4.style.backgroundColor = 'blue';
+}
+
+
+document.getElementById('s2color3').onclick = function() {
+	var calls2color3_1 = document.getElementById('fa1_sub1');
+	var calls2color3_2 = document.getElementById('fa1_Sub2');
+	var calls2color3_3 = document.getElementById('fa2_sub1');
+	var calls2color3_4 = document.getElementById('fa2_Sub2');
+	calls2color3_1.style.backgroundColor = 'orange';
+	calls2color3_2.style.backgroundColor = 'orange';
+	calls2color3_3.style.backgroundColor = 'orange';
+	calls2color3_4.style.backgroundColor = 'orange';
+}
+
+
+document.getElementById('s2color4').onclick = function() {
+	var calls2color4_1 = document.getElementById('fa1_sub1');
+	var calls2color4_2 = document.getElementById('fa1_Sub2');
+	var calls2color4_3 = document.getElementById('fa2_sub1');
+	var calls2color4_4 = document.getElementById('fa2_Sub2');
+	calls2color4_1.style.backgroundColor = 'purple';
+	calls2color4_2.style.backgroundColor = 'purple';
+	calls2color4_3.style.backgroundColor = 'purple';
+	calls2color4_4.style.backgroundColor = 'purple';
+}
+
+
+document.getElementById('s2color5').onclick = function() {
+	var calls2color5_1 = document.getElementById('fa1_sub1');
+	var calls2color5_2 = document.getElementById('fa1_Sub2');
+	var calls2color5_3 = document.getElementById('fa2_sub1');
+	var calls2color5_4 = document.getElementById('fa2_Sub2');
+	calls2color5_1.style.backgroundColor = 'pink';
+	calls2color5_2.style.backgroundColor = 'pink';
+	calls2color5_3.style.backgroundColor = 'pink';
+	calls2color5_4.style.backgroundColor = 'pink';
+}
+
+
+document.getElementById('s2color6').onclick = function() {
+	var calls2color6_1 = document.getElementById('fa1_sub1');
+	var calls2color6_2 = document.getElementById('fa1_Sub2');
+	var calls2color6_3 = document.getElementById('fa2_sub1');
+	var calls2color6_4 = document.getElementById('fa2_Sub2');
+	calls2color6_1.style.backgroundColor = 'green';
+	calls2color6_2.style.backgroundColor = 'green';
+	calls2color6_3.style.backgroundColor = 'green';
+	calls2color6_4.style.backgroundColor = 'green';
+}
+
+
