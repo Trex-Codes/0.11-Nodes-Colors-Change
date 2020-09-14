d1c16584d3c21dd8bb58ea68905b3831a8234243
juan sebastian <juan.13JSG@gmail.com>
2020-09-13 22:32:44 -0500

    2.0 CHANGE problems in btn(1-2) and missing fix the codes JS

--- Script1.js
+++ Script1.js
@@ -1,8 +1,41 @@
 // cuadroColor
 
-function ColorCuadro() {
-	const callColorSun = document.getElementById('cuadroColor');
+function FatherCall() {
+	const callColorSun = document.getElementById('cuadrofather');
 	callColorSun.style.display = 'inherit';
 	callColorSun.style.visibility = 'visible';
 
+}
+
+// call colors and and funcions
+//Father
+
+document.getElementById('fatherColor1').onclick = function() {
+	var ColorFather1 = document.getElementById('fatherNod');
+	ColorFather1.style.backgroundColor = 'red';
+}
+
+document.getElementById('fatherColor2').onclick = function() {
+	var ColorFather2 = document.getElementById('fatherNod');
+	ColorFather2.style.backgroundColor = 'blue';
+}
+
+document.getElementById('fatherColor3').onclick = function() {
+	var ColorFather3 = document.getElementById('fatherNod');
+	ColorFather3.style.backgroundColor = 'orange';
+}
+
+document.getElementById('fatherColor4').onclick = function() {
+	var ColorFather4 = document.getElementById('fatherNod');
+	ColorFather4.style.backgroundColor = 'purple';
+}
+
+document.getElementById('fatherColor5').onclick = function() {
+	var ColorFather5 = document.getElementById('fatherNod');
+	ColorFather5.style.backgroundColor = 'pink';
+}
+
+document.getElementById('fatherColor6').onclick = function() {
+	var ColorFather6 = document.getElementById('fatherNod');
+	ColorFather6.style.backgroundColor = 'green';
 }
--- Script2.js
+++ Script2.js
@@ -1,5 +1,5 @@
 function S1Call() {
-	var CallcanvasSun = document.getElementById('cuadroSun2');
+	var CallcanvasSun = document.getElementById('cuadroSon1');
 		CallcanvasSun.style.visibility = 'visible';
 		CallcanvasSun.style.display = 'block';
 }
@@ -7,42 +7,42 @@ function S1Call() {
 // call colors and and funcions
 //son1
 
-document.getElementById('s1color1').onclick = function() {
+document.getElementById('Son1Color1').onclick = function() {
 	var calls1color1_1 = document.getElementById('son1_Sub1');
 	var calls1color1_2 = document.getElementById('son1_Sub2');
 	calls1color1_1.style.backgroundColor = 'red';
 	calls1color1_2.style.backgroundColor = 'red';
 }
 
-document.getElementById('s1color2').onclick = function() {
+document.getElementById('Son1Color2').onclick = function() {
 	var calls1color2_1 = document.getElementById('son1_Sub1');
 	var calls1color2_2 = document.getElementById('son1_Sub2');
 	calls1color2_1.style.backgroundColor = 'blue';
 	calls1color2_2.style.backgroundColor = 'blue';
 }
 
-document.getElementById('s1color3').onclick = function() {
+document.getElementById('Son1Color3').onclick = function() {
 	var calls1color3_1 = document.getElementById('son1_Sub1');
 	var calls1color3_2 = document.getElementById('son1_Sub2');
 	calls1color3_1.style.backgroundColor = 'orange';
 	calls1color3_2.style.backgroundColor = 'orange';
 }
 
-document.getElementById('s1color4').onclick = function() {
+document.getElementById('Son1Color4').onclick = function() {
 	var calls1color4_1 = document.getElementById('son1_Sub1');
 	var calls1color4_2 = document.getElementById('son1_Sub2');
 	calls1color4_1.style.backgroundColor = 'purple';
 	calls1color4_2.style.backgroundColor = 'purple';
 }
 
-document.getElementById('s1color5').onclick = function() {
+document.getElementById('Son1Color5').onclick = function() {
 	var calls1color5_1 = document.getElementById('son1_Sub1');
 	var calls1color5_2 = document.getElementById('son1_Sub2');
 	calls1color5_1.style.backgroundColor = 'pink';
 	calls1color5_2.style.backgroundColor = 'pink';
 }
 
-document.getElementById('s1color6').onclick = function() {
+document.getElementById('Son1Color6').onclick = function() {
 	var calls1color6_1 = document.getElementById('son1_Sub1');
 	var calls1color6_2 = document.getElementById('son1_Sub2');
 	calls1color6_1.style.backgroundColor = 'green';
